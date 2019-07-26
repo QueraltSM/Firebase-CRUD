a9821ed55197915cae538ea06cc11a3e25e1539a
QueraltSosaMompel <queraltsosa@hotmail.com>
2019-07-26 20:39:25 +0100

    Verify email before login has been implemented

--- Firebase-CRUD/ViewController.swift
+++ Firebase-CRUD/ViewController.swift
@@ -9,18 +9,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func login(_ sender: Any) {
         Auth.auth().signIn(withEmail: email.text!, password: password.text!) { authResult, error in
-            if error != nil {
+            if let authResult = authResult {
+                let user = authResult.user
+                print("User has Signed In")
+                if user.isEmailVerified {
+                    print("User was logged")
+                    self.performSegue(withIdentifier: "HomeVCSegue", sender: nil)
+                } else {
+                    print("User must check email and verify his account")
+                }
+            } else if error != nil {
                 print("Error")
-            } else {
-                print("User was logged")
-                self.performSegue(withIdentifier: "HomeVCSegue", sender: self)
             }
         }
     }
-    
     
     @IBAction func resetPassword(_ sender: Any) {
         Auth.auth().sendPasswordReset(withEmail: email.text!) { error in
